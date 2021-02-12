591a13e685d7636aae6c3f1e083cee339c1d9e64
AbouOpenSource <aboudra1996@gmail.com>
2021-02-13 00:44:04 +0100

    commit

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
 print("Skynny")
 print("Salut tout le monde")
 print("Aujourdhui on essaie de comprendre docker")
-
-
-def test_well_format():
-    assert True == True
-
-
-def test_not_well_format():
-    assert False == False
-
-
-def test_not_well_end():
-    assert True != False
