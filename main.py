6bed18d3ba8dbc846e3a0b65cc66cffa86a91b30
AbouOpenSource <aboudra1996@gmail.com>
2021-02-13 00:41:25 +0100

    adding action push

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-
-print("Apoutchou")
+print("Skynny")
 print("Salut tout le monde")
 print("Aujourdhui on essaie de comprendre docker")
+
+
+def test_well_format():
+    assert True == True
+
+
+def test_not_well_format():
+    assert False == False
+
+
+def test_not_well_end():
+    assert True != False
